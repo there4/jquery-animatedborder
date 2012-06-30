068e450a7ef30ea53ce40f6163f0fac5531d8ed3
Craig Davis <craig@there4development.com>
2012-06-30 09:44:32 -0500

    Update grunt.js for the latest version of grunt.

--- grunt.js
+++ grunt.js
@@ -1,52 +1,80 @@
-/*global config:true, task:true*/
-config.init({
-  pkg: '<json:package.json>',
-  meta: {
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= template.today("m/d/yyyy") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-      '* Copyright (c) <%= template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-  },
-  concat: {
-    'dist/jquery.animatedborder.js': ['<banner>', '<file_strip_banner:src/jquery.animatedborder.js>']
-  },
-  min: {
-    'dist/jquery.animatedborder.min.js': ['<banner>', 'dist/jquery.animatedborder.js']
-  },
-  qunit: {
-    files: ['test/**/*.html']
-  },
-  lint: {
-    files: ['grunt.js', 'src/*.js', 'test/**/*.js']
-  },
-  watch: {
-    files: '<config:lint.files>',
-    tasks: 'lint qunit'
-  },
-  jshint: {
-    options: {
-      onevar: true,
-      curly: true,
-      eqeqeq: true,
-      immed: true,
-      latedef: true,
-      newcap: true,
-      noarg: true,
-      sub: true,
-      undef: true,
-      eqnull: true,
-      browser: true
+/*global module*/
+/*
+ * grunt
+ * https://github.com/cowboy/grunt
+ *
+ * Copyright (c) 2012 "Cowboy" Ben Alman
+ * Licensed under the MIT license.
+ * http://benalman.com/about/license/
+ */
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+
+    meta: {
+      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("m/d/yyyy") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
     },
-    globals: {
-      jQuery: true
-    }
-  },
-  uglify: {}
-});
 
-// Default task.
-task.registerTask('default', 'lint qunit concat min');
+    concat: {
+      dist: {
+        src: ['<banner:meta.banner>', 'src/jquery.animatedborder.js'],
+        dest: 'dist/jquery.animatedborder.js'
+      }
+    },
 
+    min: {
+      dist: {
+        src: ['<banner:meta.banner>', 'dist/jquery.animatedborder.js'],
+        dest: 'dist/jquery.animatedborder.min.js'
+      }
+    },
 
-task.registerTask('test', 'qunit');
+    qunit: {
+      all: ['test/**/*.html']
+    },
+
+    lint: {
+      files: ['grunt.js', 'src/*.js', 'test/**/*.js']
+    },
+
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint qunit'
+    },
+
+    jshint: {
+      options: {
+        onevar: true,
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        jQuery: true
+      }
+    },
+
+    uglify: {}
+  });
+
+  // Default task will build for deployment
+  grunt.registerTask('default', 'lint qunit concat min');
+
+  // Shortcut to standardize this between my repos, some run jasmine,
+  // and some run qunit, some run both.
+  grunt.registerTask('test', 'qunit');
+
+};
